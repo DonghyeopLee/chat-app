3ef0bac031015a523d24ce87c6064c4734100c26
Ian Lee <87459518+DonghyeopLee@users.noreply.github.com>
2024-10-25 23:49:41 +1100

    dasd

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,10 @@
 import ChatBot from '../pages/ChatBot';
 import MenuBar from '../pages/MenuBar';
-import './App.css';
+
 function App() {
 
   return (
     <>
-
       <div className="App">
         <MenuBar />
         <h1>Chatbot demo</h1>
