dbbe3a14e184032ecc9210cbe879df624e540bc5
Ian Lee <87459518+DonghyeopLee@users.noreply.github.com>
2024-10-25 23:20:21 +1100

    updatre

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -9,11 +9,9 @@ function App() {
       <div>
 
       </div>
-      <h1>Vite + React</h1>
+      <h1>Chatbot demo</h1>
       <ChatBot />
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
